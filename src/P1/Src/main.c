8c5b562aff189a2c864d33e185a9e003d0e31438
Marcel <info@prodevmo.com>
2020-04-07 23:36:04 +0200

    working p1 task

--- src/P1/Src/main.c
+++ src/P1/Src/main.c
@@ -12,10 +12,16 @@
 #define AHB1ENR 0x40023800U
 #define GPIOBEN 0x30U
 
+
 #define GPIOB 0x40020400U
 #define GPIOB_MODER 0x00U
 
 #define GPIOB_ODR 0x14U
+
+
+#define BIT0 0
+#define BIT1 1
+#define BIT7 7
 
 void delay(){
 
@@ -26,18 +32,19 @@ void delay(){
 }
 int main(void)
 {
-	//ENABLE CLOCK
-	*((volatile uint32_t*)(AHB1ENR+GPIOBEN)) |=(1 << 1);
-	//CLEAR MODE
-	*((volatile uint32_t*)(GPIOB+GPIOB_MODER)) |= (0b00000000000000000100000000000000);
+	//ENABLE CLOCK für FPIOB
+	*((volatile uint32_t*)(AHB1ENR+GPIOBEN)) |=(1 << BIT1);
+	//CLEAR MODE -> default state ist input 01-> ist output normal
+	*((volatile uint32_t*)(GPIOB+GPIOB_MODER)) |= (0b00000000000000000100000000000001); //PB7 und PB0 sind die led angeschlossen somit BIT15 = 1 BIT 16 = 0 für PB7 und BIT1 = 0 und BIT0 = 1 für PB0
 
 
 	for(;;){
 
 		delay();
-		*((volatile uint32_t*)(GPIOB+GPIOB_ODR)) |=(1 << 7);
+		*((volatile uint32_t*)(GPIOB+GPIOB_ODR)) =0b0000000000000001; //BIT7 LOW BIT0 HIGH
+		//*((volatile uint32_t*)(GPIOB+GPIOB_ODR)) &= ~(1 << BIT0);
 		delay();
-		*((volatile uint32_t*)(GPIOB+GPIOB_ODR)) &= ~(1 << 7);
-
+		//*((volatile uint32_t*)(GPIOB+GPIOB_ODR)) |= (1 << BIT0); //alternative
+		*((volatile uint32_t*)(GPIOB+GPIOB_ODR)) =0b0000000010000000; //BIT7 HIG BIT0 LOW
 	}
 }
