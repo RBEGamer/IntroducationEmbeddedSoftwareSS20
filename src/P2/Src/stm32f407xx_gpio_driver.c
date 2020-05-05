ba75993ad252e50a2968cd15a6a10cedf94c3bd3
Marcel <info@prodevmo.com>
2020-05-05 02:10:56 +0200

    sca

--- src/P2/Src/stm32f407xx_gpio_driver.c
+++ src/P2/Src/stm32f407xx_gpio_driver.c
@@ -248,13 +248,13 @@ void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
 	// Nicht alle Interrupts sind im Mikrocontroller aktiv. Überprüfen sie dazu das Handbuch (Reference Manual) des Mikrocontrollers.
 	if (EnorDi == ENABLE)
 	{
-		//ToDo: Programmieren der entsprechenden ISERx register
+
 	//	*((volatile uint32_t*)NVIC_ISER0)[(uint32_t)((int32_t)IRQNumber) >> 5] = (uint32_t)(1 << ((uint32_t)((int32_t)IRQNumber) & (uint32_t)0x1F));
 		*((volatile uint32_t*)NVIC_ISER0) |= (1<< IRQNumber);
 	//	NVIC->ICER0[IRQNumber] = 1;
 	}else{
-		*((volatile uint32_t*)NVIC_ICER0) |= (1<< IRQNumber);
-		//ToDo: Programmieren der entsprechenden ICERx register
+		*((volatile uint32_t*)NVIC_ICER0) &= ~(1<< IRQNumber);
+
 	}
 }
 
