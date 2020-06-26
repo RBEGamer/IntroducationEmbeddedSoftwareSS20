52d2ce260f2d16c6049330bb9743513e27f6e235
Marcel Ochsendorf <you@example.com>
2020-06-26 19:06:23 +0100

    executables add

---
+++ src/SRC_RPI_BUGGY_PROJECT/TASK_4_GYROSKOP/TASK_4_GYROPKOP_RECHTECK_FAHREN/TASK_4_AUFBAU_GYROPKOP_RECHTECK_FAHREN_SRC/TASK_3/build/main.cpp.d
@@ -0,0 +1,19 @@
+build/./main.cpp.o: main.cpp CompassDriver.h GyroscopeDriver.h \
+ LedDriver.h MotorDriver.h adafruitmotorhat.h pwm.h i2cdevice.h \
+ adafruitdcmotor.h
+
+CompassDriver.h:
+
+GyroscopeDriver.h:
+
+LedDriver.h:
+
+MotorDriver.h:
+
+adafruitmotorhat.h:
+
+pwm.h:
+
+i2cdevice.h:
+
+adafruitdcmotor.h:
